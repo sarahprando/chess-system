56090d437e3ac4e01baf24784bc8f90114826c6b
Sarah Prando <sarahprando40@gmail.com>
2026-07-14 19:43:45 -0300

    feat: finalize chess system including movement validation, check/checkmate logic and game flow

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -3,6 +3,7 @@ package chess;
 import boardgame.Position;
 
 public class ChessPosition {
+
 	private char column;
 	private int row;
 
@@ -23,17 +24,15 @@ public class ChessPosition {
 	}
 
 	protected Position toPosition() {
-		return new Position (8 - row, column - 'a');
+		return new Position(8 - row, column - 'a');
 	}
-	
+
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
+		return new ChessPosition((char) ('a' + position.getColumn()), 8 - position.getRow());
 	}
 
 	@Override
 	public String toString() {
 		return "" + column + row;
 	}
-	
-	
 }
